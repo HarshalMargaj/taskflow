8bdeb2829974fbd54a0340e42217de9969ec1be1
Harshal Margaj <apple@Apples-MacBook-Air.local>
2025-02-28 19:34:24 +0530

    added organization in tab

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,4 +1,13 @@
 import { OrgControl } from "./_components/orgControl";
+import { startCase } from "lodash";
+import { auth } from "@clerk/nextjs/server";
+
+export async function generateMetadata() {
+	const { orgSlug } = await auth();
+	return {
+		title: startCase(orgSlug || "organization"),
+	};
+}
 
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
 	return (

3188149e718ffebf85698ec860467d1dd706202c
Harshal Margaj <apple@Apples-MacBook-Air.local>
2025-03-03 12:42:17 +0530

    update delete query

--- actions/deleteList.ts
+++ actions/deleteList.ts
@@ -1,15 +1,22 @@
 "use server";
 
 import { db } from "@/lib/db";
-
+import { auth } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
 export async function deleteList(id: string, boardId: string) {
+	const { orgId } = await auth();
+	if (!orgId) {
+		throw new Error("Unauthorized: No organization ID found.");
+	}
 	await db.list.delete({
 		where: {
 			id,
 			boardId,
+			board: {
+				orgId,
+			},
 		},
 	});
 
